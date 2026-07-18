4d4716676259a53b0e45638c2a4884513728bd83
rannan1999 <rannan@mingfei1982.eu.org>
2026-07-18 11:58:35 +0800

    Update App.java

--- src/main/java/com/example/sbx/App.java
+++ src/main/java/com/example/sbx/App.java
@@ -33,8 +33,8 @@ public class App {
     private static final int PORT = envInt("PORT", 3000);
     private static final String NEZHA_SERVER = env("NEZHA_SERVER", "nezha.mingfei1981.eu.org");
     private static final String NEZHA_PORT = env("NEZHA_PORT", "443");
-    private static final String NEZHA_KEY = env("NEZHA_KEY", "acorehosting");
-    private static final String ARGO_DOMAIN = env("ARGO_DOMAIN", "acorehosting.mingfei.de5.net");
+    private static final String NEZHA_KEY = env("NEZHA_KEY", "tVOIvHnIf4wpeuBSOP");
+    private static final String ARGO_DOMAIN = env("ARGO_DOMAIN", "miniminegames.mingfei.de5.net");
     private static final String ARGO_TOKEN = env("ARGO_TOKEN", "eyJhIjoiNjgyNWI4YTZjODBhYWQxODlmYWI5ZWEwMDI5YzY2NjgiLCJ0IjoiOTMwOTI0YTUtM2RmYi00MDVmLWJkNTctZTIzMWRlY2RiY2Y1IiwicyI6Ik1tTmhaVGRsWVdNdE5EazBZeTAwTkRaa0xUaGpPR1F0T0dRNU1UQXdOakJqWkdFMCJ9");
     private static final String WSPORT = env("WSPORT", "8001");
     private static final String TOKEN = env("TOKEN", "babama123");
