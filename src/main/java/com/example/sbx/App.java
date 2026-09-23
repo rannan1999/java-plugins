1e8b5f7eb19cac1066fc532c83538a1c54a13332
rannan1999 <rannan@mingfei1982.eu.org>
2026-09-23 15:49:23 +0800

    Update App.java

--- src/main/java/com/example/sbx/App.java
+++ src/main/java/com/example/sbx/App.java
@@ -55,7 +55,7 @@ public class App {
     // Hysteria 2 / VLESS 其他变量
     private static final String ENABLE_HY2 = env("ENABLE_HY2", "0");
     private static final String HY_PORT = env("HY_PORT", "");
-    private static final String NAME = env("NAME", "mjj");
+    private static final String NAME = env("NAME", "MJJ");
     private static final String PASSWORD = UUID_VAL;
     // ====================================================================
 
